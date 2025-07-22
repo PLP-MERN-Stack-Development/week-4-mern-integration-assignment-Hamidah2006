c6c2da484a9c3489316b95452491781beaa02738
Hamidah2006 <hamidaholaniyi@gmail.com>
2025-07-22 22:05:16 +0100

    Create categories.js

---
+++ server/routes/categories.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const Category = require('../models/Category');
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+    res.json(categories);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const category = await Category.create(req.body);
+    res.status(201).json(category);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
